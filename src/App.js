f11913a02c3bcfa5a4b2820df8fdfacd497f092a
Gautammak <gautammakavana2001@gmail.com>
2023-08-17 13:58:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Tempapp from './components/Tempapp'
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+      <Tempapp />
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
